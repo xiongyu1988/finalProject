be4124b57dda05b504f7ce5077dce2aa086b205a
Yu Xiong <xiongyu1988@gmail.com>
2024-04-08 22:44:35 -0700

    by using shared_ptr, the multiple mountains show in game.

---
+++ build/CMakeFiles/SnakeGame.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/xiongyu1988/finalProjectUdacity/projectYu/src/controller.cpp" "CMakeFiles/SnakeGame.dir/src/controller.cpp.o" "gcc" "CMakeFiles/SnakeGame.dir/src/controller.cpp.o.d"
+  "/home/xiongyu1988/finalProjectUdacity/projectYu/src/game.cpp" "CMakeFiles/SnakeGame.dir/src/game.cpp.o" "gcc" "CMakeFiles/SnakeGame.dir/src/game.cpp.o.d"
+  "/home/xiongyu1988/finalProjectUdacity/projectYu/src/main.cpp" "CMakeFiles/SnakeGame.dir/src/main.cpp.o" "gcc" "CMakeFiles/SnakeGame.dir/src/main.cpp.o.d"
+  "/home/xiongyu1988/finalProjectUdacity/projectYu/src/moutain.cpp" "CMakeFiles/SnakeGame.dir/src/moutain.cpp.o" "gcc" "CMakeFiles/SnakeGame.dir/src/moutain.cpp.o.d"
+  "/home/xiongyu1988/finalProjectUdacity/projectYu/src/renderer.cpp" "CMakeFiles/SnakeGame.dir/src/renderer.cpp.o" "gcc" "CMakeFiles/SnakeGame.dir/src/renderer.cpp.o.d"
+  "/home/xiongyu1988/finalProjectUdacity/projectYu/src/snake.cpp" "CMakeFiles/SnakeGame.dir/src/snake.cpp.o" "gcc" "CMakeFiles/SnakeGame.dir/src/snake.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
